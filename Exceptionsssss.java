feb9c2f6f9bfc9129efb5b3623b8baf56a0d8a19
hidingbehindrainbows <117367646+hidingbehindrainbows@users.noreply.github.com>
2023-01-05 15:37:36 +0530

    Week 6

---
+++ Exceptionsssss.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+
+
+class Wrongage extends Exception
+{ int detail;
+    Wrongage(int d)
+    {
+        detail=d;
+    }
+    public String toString()
+    {
+        return "Entered Wrong age is ["+detail+"]";
+    }
+
+}
+
+class Father {
+int f;
+Scanner in=new Scanner(System.in);
+Father()
+{
+
+System.out.println("Enter father age ");
+f=in.nextInt();
+}
+void checkage() throws Wrongage
+{
+if(f<0)
+{
+throw new Wrongage(f);
+}
+System.out.println("Father age positive");
+}
+}
+
+
+class Son extends Father{
+int s;
+Scanner in=new Scanner(System.in);
+Son()
+{
+super();
+System.out.println("Enter son age ");
+s=in.nextInt();
+}
+
+void checkages() throws Wrongage
+{
+super.checkage();
+if(s<0)
+{
+throw new Wrongage(f);
+}
+System.out.println("Son age positive");
+}
+
+
+void checkage() throws Wrongage
+{
+if(s>f)
+{
+throw new Wrongage(s);
+}
+System.out.println("Father-Son age correct");
+}
+
+}
+
+
+class Exceptionsssss{
+public static void main(String args[])
+{
+
+int f,s;
+
+Father fath=new Father();
+
+Father r;
+r=fath;
+try{
+r.checkage();
+}
+catch(Wrongage e){
+System.out.println("Father age wrong"+e);
+}
+Son sn=new Son();
+r=sn;
+
+try{
+
+sn.checkages();
+r.checkage();
+}
+catch(Wrongage e){
+System.out.println("Son age wrong"+e);
+}
+}
+}
